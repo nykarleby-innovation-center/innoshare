95e924966d71a44b6096dc418a9b46acf506e9a7
Frank Sandqvist <kankki@gmail.com>
2024-09-11 14:45:26 +0300

    Fix in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,27 @@
 import { NextRequest, NextResponse } from "next/server"
 import { decodeUnverifiedSessionCookie } from "./utils/session"
 import { ENVIRONMENT } from "./utils/env"
+import { Language } from "./types/language"
 
 export function middleware(request: NextRequest) {
   const unverifiedSession = decodeUnverifiedSessionCookie()
 
+  const origin = request.headers.get("origin")
+  const referer = request.headers.get("referer")
+
+  let lang: Language = "en"
+  if (origin && referer) {
+    const path = referer.replace(origin, "")
+    lang = path.split("/")[1] as Language
+  }
+
   if (
     unverifiedSession?.userOnboarded === false &&
-    request.nextUrl.pathname !== "/en/user-settings"
+    request.nextUrl.pathname !== `/${lang}/user-settings`
   ) {
-    return NextResponse.redirect(new URL("/en/user-settings", ENVIRONMENT.HOST))
+    return NextResponse.redirect(
+      new URL(`/${lang}/user-settings`, ENVIRONMENT.HOST)
+    )
   }
 
   return NextResponse.next()
